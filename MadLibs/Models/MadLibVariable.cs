8e1e1410185fd2bfcac28a2770e8519286f191f6
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-20 18:18:19 -0800

    Add properties and get/set methods to MadLibVariable

--- MadLibs/Models/MadLibVariable.cs
+++ MadLibs/Models/MadLibVariable.cs
@@ -2,15 +2,128 @@ namespace MadLibs.Models
 {
   public class MadLibVariable
   {
-    // set private strings here for the different inputs gathered in the madlib
+    private string _noun1;
+    private string _noun2;
+    private string _noun3;
+    private string _noun4;
+    private string _noun5;
+    private string _noun6;
+    private string _pluralNoun1;
+    private string _pluralNoun2;
+    private string _adjective1;
+    private string _adjective2;
+    private string _adjective3;
+    private string _number1;
+    private string _number2;
 
-    public string Get____()
+    public string GetNoun1()
     {
-      return _______;
+      return _noun1;
     }
-    private void Set_______(string new______)
+    private void SetNoun1(string newNoun)
     {
-      _______ = _______
+      _noun1 = newNoun;
+    }
+
+    public string GetNoun2()
+    {
+      return _noun2;
+    }
+    private void SetNoun2(string newNoun)
+    {
+      _noun2 = newNoun;
+    }
+
+    public string GetNoun3()
+    {
+      return _noun3;
+    }
+    private void SetNoun3(string newNoun)
+    {
+      _noun3 = newNoun;
+    }
+
+    public string GetNoun4()
+    {
+      return _noun4;
+    }
+    private void SetNoun4(string newNoun)
+    {
+      _noun4 = newNoun;
+    }
+    public string GetNoun5()
+    {
+      return _noun5;
+    }
+    private void SetNoun5(string newNoun)
+    {
+      _noun5 = newNoun;
+    }
+    public string GetNoun6()
+    {
+      return _noun6;
+    }
+    private void SetNoun6(string newNoun)
+    {
+      _noun6 = newNoun;
+    }
+    public string GetPluralNoun1()
+    {
+      return _pluralNoun1;
+    }
+    private void SetPluralNoun1(string newNoun)
+    {
+      _pluralNoun1 = newNoun;
+    }
+    public string GetPluralNoun2()
+    {
+      return _pluralNoun2;
+    }
+    private void SetPluralNoun2(string newNoun)
+    {
+      _pluralNoun2 = newNoun;
+    }
+
+    public string GetAdjective1()
+    {
+      return _adjective1;
+    }
+    public void SetAdjective1(string newAdjective)
+    {
+      _adjective1 = newAdjective;
+    }
+    public string GetAdjective2()
+    {
+      return _adjective2;
+    }
+    public void SetAdjective2(string newAdjective)
+    {
+      _adjective2 = newAdjective;
+    }
+    public string GetAdjective3()
+    {
+      return _adjective3;
+    }
+    public void SetAdjective3(string newAdjective)
+    {
+      _adjective3 = newAdjective;
+    }
+
+    public string GetNumber1()
+    {
+      return _number1;
+    }
+    public void SetNumber1(string newNumber)
+    {
+      _number1 = newNumber;
+    }
+    public string GetNumber2()
+    {
+      return _number2;
+    }
+    public void SetNumber2(string newNumber)
+    {
+      _number2 = newNumber;
     }
   }
 }
